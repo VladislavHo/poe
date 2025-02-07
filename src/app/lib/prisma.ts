5fa5a6a6b4c54f1b7099da5f773e27f8c4f5f471
Vladislav <initrowlad@gmail.com>
2025-02-07 20:48:16 +0300

    update prisma.ts, show info error warn

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -7,7 +7,9 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  })
 }
 
 declare const globalThis: {
